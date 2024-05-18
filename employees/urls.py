93f12068286a5e1c1fe3537c34a29b0c0ed3db8f
Marcos Serra <marcosserra133@gmail.com>
2024-05-18 14:55:02 -0300

    refactor: update URL to use CBV for HomeView
    Alterada a URL de home_view (FBV) para HomeView.as_view() (CBV).

--- employees/urls.py
+++ employees/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from employees.views import home_view, new_person_view, list_persons_view, new_address_view, new_contact_view, new_formofpay_view, employee_detail_view
+from employees.views import HomeView, new_person_view, list_persons_view, new_address_view, new_contact_view, new_formofpay_view, employee_detail_view
 
 urlpatterns = [
-    path('', home_view, name='home'),
+    path('', HomeView.as_view(), name='home'),
     path('employee', list_persons_view, name='employee'),  # listagem de funcionários
     path('employee/<int:pk>/', employee_detail_view, name='employee_details'),  # caminho para abrir detalhes sobre o funcionario 
     path('register/', new_person_view, name='register_person'),  # registrar funcionário
