446fb706f11f3c80bdb4a63bfcf2a6a4b652ab2a
Marcos Serra <marcosserra133@gmail.com>
2024-05-18 16:13:21 -0300

    refactor: update URL to use CBV for EmployeeCreateView
    Alterada a URL de new_person_view (FBV) para refactor: update URL to use CBV for EmployeeCreateView.as_view() (CBV).

--- employees/urls.py
+++ employees/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from employees.views import HomeView, EmployeesListView, EmployeesDetailView, new_person_view, new_address_view, new_contact_view, new_formofpay_view
+from employees.views import HomeView, EmployeesListView, EmployeesDetailView, EmployeeCreateView, new_person_view, new_address_view, new_contact_view, new_formofpay_view
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('employee/', EmployeesListView.as_view(), name='employee'),  # listagem de funcionários
     path('employee/<int:pk>/', EmployeesDetailView.as_view(), name='employee_details'),  # caminho para abrir detalhes sobre o funcionario 
-    path('register/', new_person_view, name='register_person'),  # registrar funcionário
+    path('register/', EmployeeCreateView.as_view(), name='register_person'),  # registrar funcionário
     path('address/', new_address_view, name='register_address'),  # registrar endereço
     path('contact/', new_contact_view, name='register_contact'),  # registrar contato
     path('form_of_pay/', new_formofpay_view, name='register_formofpayment'), # registrar forma de pagemnto
