9bfc1f6b0b6ae9506c83c67ab20b5369edb7cbe0
Marcos Serra <marcosserra133@gmail.com>
2024-05-18 15:49:56 -0300

    refactor: update URL to use CBV for EmployeesDetailView
    Alterada a URL de employee_detail_view (FBV) para EmployeesDetailView.as_view() (CBV).

--- employees/urls.py
+++ employees/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from employees.views import HomeView, EmployeesListView, new_person_view, new_address_view, new_contact_view, new_formofpay_view, employee_detail_view
+from employees.views import HomeView, EmployeesListView, EmployeesDetailView, new_person_view, new_address_view, new_contact_view, new_formofpay_view
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('employee', EmployeesListView.as_view(), name='employee'),  # listagem de funcionários
-    path('employee/<int:pk>/', employee_detail_view, name='employee_details'),  # caminho para abrir detalhes sobre o funcionario 
+    path('employee/', EmployeesListView.as_view(), name='employee'),  # listagem de funcionários
+    path('employee/<int:pk>/', EmployeesDetailView.as_view(), name='employee_details'),  # caminho para abrir detalhes sobre o funcionario 
     path('register/', new_person_view, name='register_person'),  # registrar funcionário
     path('address/', new_address_view, name='register_address'),  # registrar endereço
     path('contact/', new_contact_view, name='register_contact'),  # registrar contato
