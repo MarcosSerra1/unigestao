92f0de99ab1935c9d2e30f151946bf5bc6dcf810
Marcos Serra <marcosserra133@gmail.com>
2024-05-18 17:07:08 -0300

    refactor: update URL to use CBV

--- employees/urls.py
+++ employees/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from employees.views import HomeView, EmployeesListView, EmployeesDetailView, EmployeeCreateView, AddressCreateView, new_contact_view, new_formofpay_view
+from employees.views import HomeView, EmployeesListView, EmployeesDetailView, EmployeeCreateView, AddressCreateView, ContactCreateView, PayCreateView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
@@ -7,6 +7,6 @@ urlpatterns = [
     path('employee/<int:pk>/', EmployeesDetailView.as_view(), name='employee_details'),  # caminho para abrir detalhes sobre o funcionario 
     path('register/', EmployeeCreateView.as_view(), name='register_person'),  # registrar funcionário
     path('register/address/', AddressCreateView.as_view(), name='register_address'),  # registrar endereço
-    path('register/contact/', new_contact_view, name='register_contact'),  # registrar contato
-    path('register/form_of_pay/', new_formofpay_view, name='register_formofpayment'), # registrar forma de pagemnto
+    path('register/contact/', ContactCreateView.as_view(), name='register_contact'),  # registrar contato
+    path('register/form_of_pay/', PayCreateView.as_view(), name='register_formofpayment'), # registrar forma de pagemnto
 ]
