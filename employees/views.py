2856774a8e3daf3a5dc74d29edecaa1780175964
Marcos Serra <marcosserra133@gmail.com>
2024-05-28 21:09:54 -0300

    ♻️ refactor: remove obsolete views and add new views for the new employee registration feature

--- employees/views.py
+++ employees/views.py
@@ -1,10 +1,11 @@
 from django.views import View
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from employees.forms import PersonModelForm, AddressModelForm, ContactInfoModelForm, FormOfPaymentModelForm
 from employees.models import Person
 from django.db import transaction
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 
 class HomeView(View):
@@ -33,56 +34,23 @@ class EmployeesDetailView(DetailView):
     template_name = 'employees/employee_details.html'
 
 
-class EmployeeCreateView(CreateView):
-    model = Person
-    form_class = PersonModelForm
-    template_name = 'employees/register_person.html'
-    success_url = '/register/address/'
+class CreateEmployeeView(View):
+    template_name = 'employees/register_employee.html'
+    
+    def get(self, request):
+        person_form = PersonModelForm()
+        contact_info_form = ContactInfoModelForm()
+        address_form = AddressModelForm()
+        form_of_payment_form = FormOfPaymentModelForm()
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['new_person_form'] = context['form']
-        return context
+        return render(request, self.template_name, {
+            'person_form': person_form,
+            'contact_info_form': contact_info_form,
+            'address_form': address_form,
+            'form_of_payment_form': form_of_payment_form
+        })
 
-
-class AddressCreateView(CreateView):
-    model = Person
-    form_class = AddressModelForm
-    template_name = 'employees/register_address.html'
-    success_url = '/register/contact/'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['new_address_form'] = context['form']
-        return context
-
-
-class ContactCreateView(CreateView):
-    model = Person
-    form_class = ContactInfoModelForm
-    template_name = 'employees/register_contact.html'
-    success_url = '/register/form_of_pay/'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['new_contact_form'] = context['form']
-        return context
-
-
-class PayCreateView(CreateView):
-    model = Person
-    form_class = FormOfPaymentModelForm
-    template_name = 'employees/register_form_payment.html'
-    success_url = '/employee/'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['new_formofpay_form'] = context['form']
-        return context
-
-
-def create_employee(request):
-    if request.method == 'POST':
+    def post(self, request):
         person_form = PersonModelForm(request.POST)
         contact_info_form = ContactInfoModelForm(request.POST)
         address_form = AddressModelForm(request.POST)
@@ -104,16 +72,11 @@ def create_employee(request):
                 form_of_payment.employee = person
                 form_of_payment.save()
                 
-            return redirect('success_url')  # Redirecione para uma página de sucesso
-    else:
-        person_form = PersonModelForm()
-        contact_info_form = ContactInfoModelForm()
-        address_form = AddressModelForm()
-        form_of_payment_form = FormOfPaymentModelForm()
+            return redirect(reverse_lazy('/employee/'))  # Redirecione para uma página de sucesso
 
-    return render(request, 'employees/create_employee.html', {
-        'person_form': person_form,
-        'contact_info_form': contact_info_form,
-        'address_form': address_form,
-        'form_of_payment_form': form_of_payment_form
-    })
+        return render(request, self.template_name, {
+            'person_form': person_form,
+            'contact_info_form': contact_info_form,
+            'address_form': address_form,
+            'form_of_payment_form': form_of_payment_form
+        })
