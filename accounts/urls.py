8bc1857a53c9bc1eb524eadde6f4713986ff8acf
Marcos Serra <marcosserra133@gmail.com>
2024-06-03 21:10:18 -0300

    🔄 refactor: Removal of the registration route and its import into the accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from accounts.views import RegisterView, LoginView, LogoutView
+from accounts.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 ]
