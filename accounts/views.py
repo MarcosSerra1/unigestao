f7d1f901773748aa7d410718f3402dc3808baf58
Marcos Serra <marcosserra133@gmail.com>
2024-06-03 21:12:17 -0300

    🔄 refactor: Removal of RegisterView and its imports in the accounts app
    Removida a classe RegisterView e suas importações relacionadas no arquivo views.py do app accounts.

--- accounts/views.py
+++ accounts/views.py
@@ -1,19 +1,8 @@
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse_lazy
 from django.views.generic import FormView, RedirectView
-from django.views import View
-from django.shortcuts import redirect, render
-
-
-class RegisterView(FormView):
-    template_name = 'accounts/register.html'
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-
-    def form_valid(self, form):
-        form.save()
-        return super().form_valid(form)
+from django.shortcuts import redirect
 
 
 class LoginView(FormView):
