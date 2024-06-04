5a06ed52341ead1bc98650d5b1675458942bd687
Marcos Serra <marcosserra133@gmail.com>
2024-06-04 20:16:37 -0300

    🔄 refactor: LoginView refactoring for code improvements and maintenance
    Refatoração da LoginView para melhorar a clareza do código, otimizar a manutenção e aderir às melhores práticas de desenvolvimento Django.

--- accounts/views.py
+++ accounts/views.py
@@ -8,11 +8,11 @@ from django.shortcuts import redirect
 class LoginView(FormView):
     template_name = 'accounts/login.html'
     form_class = AuthenticationForm
-    success_url = reverse_lazy('cars_list')
+    success_url = reverse_lazy('home')
 
     def form_valid(self, form):
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password')
+        username = form.cleaned_data.get('username', None)
+        password = form.cleaned_data.get('password', None)
         user = authenticate(self.request, username=username, password=password)
         if user is not None:
             login(self.request, user)
