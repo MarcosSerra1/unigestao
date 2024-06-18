d40332c7a900b18b9221619180df5c334d1e6c39
Marcos Serra <marcosserra133@gmail.com>
2024-06-18 20:52:42 -0300

    🔧 refactor: Ajuste nos caminhos de arquivos estáticos e media no settings.py
    Refatorado o arquivo settings.py para melhorar a organização dos caminhos dos arquivos estáticos e de mídia:
    - Removido DATA_DIR e ajustado STATIC_ROOT para usar BASE_DIR diretamente.
    - Ajustado MEDIA_ROOT e STATIC_ROOT para utilizar os caminhos corretos dentro de BASE_DIR.

--- app/settings.py
+++ app/settings.py
@@ -26,7 +26,6 @@ if os.path.exists(dotenv_path):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -148,14 +147,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = DATA_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'docs/assets'),
 ]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
